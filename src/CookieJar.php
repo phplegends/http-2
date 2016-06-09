8664f9b93e88a504293f7d80ba300a34a534ebe7
Wallace de Souza Vizerra <wallacemaxters@gmail.com>
2016-06-09 14:11:51 -0300

    change setcookie method

--- src/CookieJar.php
+++ src/CookieJar.php
@@ -2,32 +2,85 @@
 
 namespace PHPLegends\Http;
 
-use PHPLegends\Collection\Collection;
+use PHPLegends\Collections\Collection;
 
+/**
+ * Não gosto do nome, mas vou deixar por padrão :(
+ * 
+ * @author Wallace de Souza Vizerra <wallacemaxters@gmail.com>
+ * 
+ * */
 class CookieJar extends Collection
 {   
+    
+    /**
+     *
+     * 
+     * @param string $name
+     * @param string $value
+     * @param \DateTime|string|integer
+     * @param null|string $path
+     * @param null|string $domain
+     * @param boolean $secure
+     * @param boolean $httpOnly
+     * */
     public function setCookie(
         $name, $value, $expires = 0, 
         $path = null, $domain = null, 
         $secure = false, $httpOnly = false
     ) {
 
-        return $this->add(new Cookie($name, $value, $expires, $path, $domain, $secure, $httpOnly));
+        $args = compact(
+            'domain',
+            'expires',
+            'httpOnly',
+            'path',
+            'secure'
+        );
+
+        return parent::set($name, Cookie::create($name, $value, $args));
     }
 
+    /**
+     * Add cookie in jar
+     * 
+     * @param Cookie $cookie
+     * @throws \InvalidArgumentException
+     * @return self
+     * */
     public function add($cookie)
     {
         if ($cookie instanceof Cookie)
         {
-            return parent::add($cookie);
+            return parent::set($cookie->getName(), $cookie);
         }
 
         throw new \InvalidArgumentException('argument of add must be Cookie');
     }
 
+    /**
+     * Sets the cookie in jar
+     * 
+     * @param string $name
+     * @param string $value
+     * */
     public function set($name, $value)
     {
         return $this->setCookie($name, $value);
+    }
+
+    /**
+     * 
+     * @param Cookie[] $items
+     * @return self
+     * */
+    public function setItems(array $items)
+    {
+        foreach ($items as $cookie) {
+            $this->add($cookie);
+        }
+
+        return $this;
     }
 
 }
