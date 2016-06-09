dde869a9b1fad8c5dec68cdc58b46abf42b50cd5
Wallace de Souza Vizerra <wallacemaxters@gmail.com>
2016-06-09 14:44:17 -0300

    fix header definitio

--- src/JsonResponse.php
+++ src/JsonResponse.php
@@ -2,13 +2,21 @@
 
 namespace PHPLegends\Http;
 
-
 class JsonResponse extends Response
-{
-	public function __construct($content, $code = 200, $options = 0, array $headers = [])
+{   
+    
+    /**
+     * 
+     * 
+     * @param mixed $content
+     * @param int $code
+     * @param int $options
+     * @param mixed $headers
+     * */
+	public function __construct($content, $code = 200, $options = 0, $headers = [])
 	{
 		parent::__construct(json_encode($content, $options), $code, $headers);
 		
-		$this->setHeader('Content-Type', 'application/json');
+		$this->getHeaders()->set('Content-Type', 'application/json');
 	}
 }
