989c237b3ac3433b957eef97053762269526fbd8
Guilherme Nascimento <brcontainer@yahoo.com.br>
2016-06-09 22:23:13 -0300

    Criado "cache" com static para micro-otimização
    Isto pode ser dispensável, mas em casos que existem loops que podem variar e acaso houver mais chamadas desta função usar uma variável estática pode ajudar no tempo da entrega do resultado.

--- src/functions.php
+++ src/functions.php
@@ -1,21 +1,26 @@
 <?php
 
-
 namespace PHPLegends\Http;
 
-function get_all_headers() 
-{ 
-	$headers = []; 
+function get_all_headers()
+{
+    static $headers;
 
-	foreach ($_SERVER as $name => $value) { 
+    if (!empty($headers)) {
+        return $headers;
+    }
 
-		if (substr($name, 0, 5) === 'HTTP_') {
+    $headers = [];
 
-			$header_name = mb_convert_case(strtr(substr($name, 5), ['_' => '-']), MB_CASE_TITLE);
+    foreach ($_SERVER as $name => $value) {
 
-			$headers[$header_name] = $value; 
-		} 
-	} 
+        if (substr($name, 0, 5) === 'HTTP_') {
 
-	return $headers; 
+            $header_name = mb_convert_case(strtr(substr($name, 5), ['_' => '-']), MB_CASE_TITLE);
+
+            $headers[$header_name] = $value;
+        }
+    }
+
+    return $headers;
 }
