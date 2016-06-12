20788efaf8fb298936786b1b41d06377f490401d
Wallace de Souza <wallacemaxters@gmail.com>
2016-06-12 19:20:31 -0300

    Type cast methods adding in ParameterCollection

--- src/ParameterCollection.php
+++ src/ParameterCollection.php
@@ -9,4 +9,81 @@ use PHPLegends\Collections\Collection;
  * @author Wallace de Souza Vizerra <wallacemaxters@gmail.com>
  * */
 
-class ParameterCollection extends Collection {}
+class ParameterCollection extends Collection 
+{   
+    /**
+     * 
+     * @param string $key
+     * @param string $default 
+     * @return string
+     * */
+    public function getAsString($key, $default = '')
+    {
+        $value = $this->getOrDefault($key, $default);
+
+        if (is_array($value)) {
+            $this->throwExceptionForInvalidType($key, 'string');
+        }
+
+        return (string) $value;
+    }
+
+    /**
+     * 
+     * @param string 
+     * @param boolean $default
+     * @return boolean
+     * */
+    public function getAsBoolean($key, $default = false)
+    {
+        return filter_var($this->getOrDefault($key, $default), FILTER_VALIDATE_BOOLEAN);
+    }
+
+    public function getAsArray($key, array $default = [])
+    {
+        return (array) $this->getOrDefault($key, $default);
+    }
+
+    public function getAsInt($key, $default = 0)
+    {
+        $value = $this->getOrDefault($key, $default);
+
+        if (is_array($value)) {
+
+            $this->throwExceptionForInvalidType($key, 'float');
+        }
+
+        return (float) $value;
+    }
+
+    public function getAsFloat($key, $default = 0)
+    {
+        $value = $this->getOrDefault($key, $default);
+
+        if (is_array($value)) {
+
+            $this->throwExceptionForInvalidType($key, 'integer');
+        }
+
+        return (float) $value;
+    }
+
+    protected function isEmptyOrNullString($key)
+    {
+        $value = $this->get($key);
+
+        return null === $value || (string) $value === '';
+    }
+
+    protected function throwExceptionForInvalidType($key, $typeExpected)
+    {
+        $message = sprintf(
+            'The item "%s" must be of "%s" type, "%s" given',
+            $key,
+            $typeExpected,
+            gettype($this->getOrDefault($key))
+        );
+
+        throw new \UnexpectedValueException($message);
+    }
+} 
