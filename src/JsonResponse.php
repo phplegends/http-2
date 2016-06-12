56b30e676250c2b155cfec78881355084b907c0d
Wallace de Souza <wallacemaxters@gmail.com>
2016-06-12 19:38:07 -0300

    fix construct problem after setContents changing

--- src/JsonResponse.php
+++ src/JsonResponse.php
@@ -18,9 +18,9 @@ class JsonResponse extends Response
      * @param int $options
      * @param mixed $headers
      * */
-	public function __construct($content, $code = 200, $options = 0, $headers = [])
+	public function __construct($content, $code = 200, $headers = [])
 	{
-		parent::__construct(json_encode($content, $options), $code, $headers);
+		parent::__construct($content, $code, $headers);
 		
 		$this->getHeaders()->set('Content-Type', 'application/json');
 	}
