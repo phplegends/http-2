4d0747b0758f2ed5c57cd2013a7508fd41f187cd
Wallace de Souza <wallacemaxters@gmail.com>
2016-06-12 19:16:52 -0300

    Invalidate setContent method in RedirectResponse class

---
+++ src/RedirectResponse.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PHPLegends\Http;
+
+/**
+ * Represents the redirect responses
+ * 
+ * @author Wallace de Souza Vizerra <wallacemaxters@gmail.com>
+ * */
+
+class RedirectResponse extends Response
+{
+	public function __construct($location, $code = 302, $headers = [])
+	{
+
+		$headers = ['Location' => (string) $location] + $headers;
+
+		$this->setStatusCode($code);
+
+		$this->resolveHeaderValue($headers);
+
+	}
+
+	/**
+	 * 
+	 * @throws \BadMethodCallException
+	 * 
+	 * */
+	public function setContent($content)
+	{
+		throw new \BadMethodCallException('Cannot set content in redirect response');
+	}
+
+	/**
+	 * In redirector response, the method send cannot 'echos' output
+	 * @return void
+	 * */
+	public function send()
+	{
+
+		if (headers_sent()) {
+
+			throw new \RunTimeException('Cannot redirect after output contents');
+		}
+
+		$this->sendHeaders();
+		$this->sendCookies();
+	}
+
+	public function getLocation()
+	{
+		return $this->getHeaders()->getLine('Location');
+	}
+
+
+}
