70c68e76c94ab56526688c9a306f66e3ea05a05e
Wallace de Souza <wallacemaxters@gmail.com>
2016-07-09 20:17:06 -0300

    improve tests

--- test/UriTest.php
+++ test/UriTest.php
@@ -9,10 +9,95 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->uri = new Uri('http://localhost/path?name=Wallace');
     }
 
+    public function testPort()
+    {
+        $this->assertEquals(80, $this->uri->getPort());
+    }
+
     public function testPath()
     {
         $this->assertEquals('/path', $this->uri->getPath());
     }
+
+    public function testHost()
+    {
+        $this->assertEquals('localhost', $this->uri->getHost());
+    }
+
+    public function testGetScheme()
+    {
+        $this->assertEquals('http', $this->uri->getScheme());
+    }
+
+    public function testGetAuthority()
+    {
+        $uri = clone $this->uri;
+
+        $this->assertEquals('localhost', $uri->getAuthority()); 
+
+        $uri->setUserInfo('wallace');
+
+        $this->assertEquals('wallace@localhost', $uri->getAuthority()); 
+
+        $uri->setUserInfo('wallace', 'my-secret-password');
+
+        $this->assertEquals('wallace:my-secret-password@localhost', $uri->getAuthority()); 
+
+        $this->assertEquals('wallace:my-secret-password@localhost:80', $uri->getAuthority(true)); 
+
+        $uri->setUserInfo('wallacemaxters@gmail.com', 'pass');
+
+        $this->assertEquals(
+            'wallacemaxters%40gmail.com:pass@localhost:80', $uri->getAuthority(true)
+        );
+    }
+
+    public function testGetHostWithPort()
+    {
+        $this->assertEquals('localhost', $this->uri->getHostWithPort());
+
+        $this->assertEquals('localhost:80', $this->uri->getHostWithPort(true));
+
+        $old_port = $this->uri->getPort();
+
+        $this->uri->setPort(8080);
+
+        $this->assertEquals('localhost:8080', $this->uri->getHostWithPort());
+
+        // normalize to state of setUP
+
+        $this->uri->setPort($old_port);
+
+    }
+
+    public function testGetHostWithScheme()
+    {
+        $this->assertEquals('http://localhost', $this->uri->GetHostWithScheme());
+
+        $this->assertEquals('http://localhost:80', $this->uri->GetHostWithScheme(true));
+
+        $old_scheme = $this->uri->getScheme();
+
+        $this->uri->setScheme('https');
+
+        $this->assertEquals('https://localhost', $this->uri->GetHostWithScheme());
+
+        $this->assertEquals('https://localhost:443', $this->uri->GetHostWithScheme(true));
+
+        $old_port = $this->uri->getPort();
+
+        $this->uri->setPort(7070);
+
+        $this->assertEquals('https://localhost:7070', $this->uri->getHostWithScheme(false));
+
+        $this->assertEquals('https://localhost:7070', $this->uri->getHostWithScheme(true));
+
+        $this->uri->setScheme($old_scheme)
+                  ->setPort($old_port);
+
+    }
+
+
 
     public function testQuery()
     {
